57eecf11ffb41399d0c587e5799a20b7b33b4baf
hosu <qxvop@iium.co.kr>
2023-09-04 16:50:19 +0900

    초기 세팅 업로드

---
+++ www/data/dbconfig.php
@@ -0,0 +1,8 @@
+<?php
+if(!defined('_BLUEVATION_')) exit;
+define('BV_MYSQL_HOST', 'localhost');
+define('BV_MYSQL_USER', 'iium01_nalgaem');
+define('BV_MYSQL_PASSWORD', 'buster@4868');
+define('BV_MYSQL_DB', 'iium01_nalgaem');
+define('BV_MYSQL_SET_MODE', false);
+?>
