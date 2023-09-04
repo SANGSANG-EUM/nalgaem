236c2301a5766f8570ddf744829b3a8008ebae14
hosu <qxvop@iium.co.kr>
2023-09-04 17:40:55 +0900

    헤더 업로드

--- www/theme/basic/head.skin.php
+++ www/theme/basic/head.skin.php
@@ -7,230 +7,87 @@ if(defined('_INDEX_')) { // index에서만 실행
 ?>
 
 <div id="wrapper">
-	<div id="header">
-		<?php if(!get_cookie("ck_hd_banner")) { // 상단 큰배너 ?>
-		<div id="hd_banner">
-			<?php if($banner1 = display_banner_bg(1, $pt_id)) { // 배너가 있나? ?>
-			<?php echo $banner1; ?>
-			<img src="<?php echo BV_IMG_URL; ?>/bt_close.gif" id="hd_close">
-			<?php } // banner end ?>
-		</div>
-		<?php } // cookie end ?>
-		<div id="tnb">
-			<div id="tnb_inner">
-				<ul class="fr">
-					<?php
-					$tnb = array();
-					if($is_admin)
-						$tnb[] = '<li><a href="'.$is_admin.'" target="_blank" class="fc_eb7">관리자</a></li>';
-					if($is_member) {
-						$tnb[] = '<li><a href="'.BV_BBS_URL.'/logout.php">로그아웃</a></li>';
-					} else {
-						$tnb[] = '<li><a href="'.BV_BBS_URL.'/login.php?url='.$urlencode.'">로그인</a></li>';
-						$tnb[] = '<li><a href="'.BV_BBS_URL.'/register.php">회원가입</a></li>';
-					}
-					$tnb[] = '<li><a href="'.BV_SHOP_URL.'/mypage.php">마이페이지</a></li>';
-					$tnb[] = '<li><a href="'.BV_SHOP_URL.'/cart.php">장바구니<span class="ic_num">'. get_cart_count().'</span></a></li>';
-					$tnb[] = '<li><a href="'.BV_SHOP_URL.'/orderinquiry.php">주문/배송조회</a></li>';
-					$tnb[] = '<li><a href="'.BV_BBS_URL.'/faq.php?faqcate=1">고객센터</a></li>';
-					$tnb_str = implode(PHP_EOL, $tnb);
-					echo $tnb_str;
-					?>
-				</ul>
-			</div>
-		</div>
-		<div id="hd">
-			<!-- 상단부 영역 시작 { -->
-			<div id="hd_inner">
-				<div class="hd_bnr">
-					<span><?php echo display_banner(2, $pt_id); ?></span>
-				</div>
-				<h1 class="hd_logo">
-					<?php echo display_logo(); ?>
-				</h1>
-				<div id="hd_sch">
-					<fieldset class="sch_frm">
-						<legend>사이트 내 전체검색</legend>
-						<form name="fsearch" id="fsearch" method="post" action="<?php echo BV_SHOP_URL; ?>/search.php" onsubmit="return fsearch_submit(this);" autocomplete="off">
-						<input type="hidden" name="hash_token" value="<?php echo BV_HASH_TOKEN; ?>">
-						<input type="text" name="ss_tx" class="sch_stx" maxlength="20" placeholder="검색어를 입력해주세요">
-						<button type="submit" class="sch_submit fa fa-search" value="검색"></button>
-						</form>
-						<script>
-						function fsearch_submit(f){
-							if(!f.ss_tx.value){
-								alert('검색어를 입력하세요.');
-								return false;
-							}
-							return true;
-						}
-						</script>
-					</fieldset>
-				</div>
-			</div>
-			<div id="gnb">
-				<div id="gnb_inner">
-					<div class="all_cate">
-						<span class="allc_bt"><i class="fa fa-bars"></i> 전체카테고리</span>
-						<div class="con_bx">
-							<ul>
-							<?php
-							$mod = 5;
-							$res = sql_query_cgy('all');
-							for($i=0; $row=sql_fetch_array($res); $i++) {
-								$href = BV_SHOP_URL.'/list.php?ca_id='.$row['catecode'];
+  <header class="header black">
+    <div class="hd-wr">
+      <a href="/" class="hd-logo">
+        <img src="/source/img/logo_black.png" alt="날갬" class="hd-black">
+        <img src="/source/img/logo_white.png" alt="날갬" class="hd-white">
+      </a>
+      <div class="hd-menu">
+        <div class="gnb-tnb-wr">
+          <ul class="hd-gnb-ul">
+            <li class="hd-gnb-li">
+              <a href="">날갬 TOUR</a>
+            </li>
+            <li class="hd-gnb-li">
+              <a href="">날갬 with 카페</a>
+            </li>
+            <li class="hd-gnb-li">
+              <a href="">내 위치 검색</a>
+            </li>
+            <li class="hd-gnb-li">
+              <a href="">소통해요 날갬</a>
+            </li>
+          </ul>
+          <ul class="hd-tnb-ul">
+            <li class="hd-tnb-li hd-tnb-li--sch">
+              <button type="button" class="hd-sch-open"><img src="/source/img/icon-search_black.png" alt="검색"></button>
+              <form action="" class="hd-sch-form">
+                <input type="text" class="hd-sch" placeholder="여행지 또는 궁금하신 정보를 입력해주세요.">
+                <button type="submit" class="hd-sch-btn"><img src="/source/img/icon-search_sky.png" alt="검색"></button>
+              </form>
+            </li>
+            <li class="hd-tnb-li hd-tnb-li--login">
+              <a href=""><img src="/source/img/icon-login_black.png" alt="" class="hd-black"><img
+                  src="/source/img/icon-login_white.png" alt="" class="hd-white">로그인</a>
+            </li>
+            <li class="hd-tnb-li hd-tnb-li--lang">
+              <button type="button" class="hd-lang-btn"><img src="/source/img/icon-lang_black.png" alt=""
+                  class="hd-black hd-lang-img"><img src="/source/img/icon-lang_white.png" alt=""
+                  class="hd-white hd-lang-img">KOR<img src="/source/img/icon-arrowlang_black.png" alt=""
+                  class="hd-black hd-langarrow-img"><img src="/source/img/icon-arrowlang_white.png" alt=""
+                  class="hd-white hd-langarrow-img"></button>
+              <ul class="hd-lang-ul">
+                <li class="hd-lang-li">
+                  <a href="">ENG</a>
+                </li>
+                <li class="hd-lang-li">
+                  <a href="">CHN</a>
+                </li>
+                <li class="hd-lang-li">
+                  <a href="">JPN</a>
+                </li>
+              </ul>
+              <!-- 구글 언어 선택 셀렉트박스 삭제 -->
+              <!-- <div id="google_translate_element"></div>
+              <script src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+              <script type="text/javascript">
+                function googleTranslateElementInit() {
+                  new google.translate.TranslateElement({
+                    pageLanguage: 'ko',
+                    autoDisplay: false
+                  }, 'google_translate_element');
+                }
+              </script> -->
+            </li>
+            <li class="hd-tnb-li hd-tnb-li--store">
+              <a href="">입점 신청<img src="/source/img/icon-link.png" alt=""></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <button type="button" class="hd-ham"></button>
+    </div>
+  </header>
 
-								if($i && $i%$mod == 0) echo "</ul>\n<ul>\n";
-							?>
-								<li class="c_box">
-									<a href="<?php echo $href; ?>" class="cate_tit"><?php echo $row['catename']; ?></a>
-									<?php
-									$r = sql_query_cgy($row['catecode'], 'COUNT');
-									if($r['cnt'] > 0) {
-									?>
-									<ul>
-										<?php
-										$res2 = sql_query_cgy($row['catecode']);
-										while($row2 = sql_fetch_array($res2)) {
-											$href2 = BV_SHOP_URL.'/list.php?ca_id='.$row2['catecode'];
-										?>
-										<li><a href="<?php echo $href2; ?>"><?php echo $row2['catename']; ?></a></li>
-										<?php } ?>
-									</ul>
-									<?php } ?>
-								</li>
-							<?php
-							}
-							$li_cnt = ($i%$mod);
-							if($li_cnt) { // 나머지 li
-								for($i=$li_cnt; $i<$mod; $i++)
-									echo "<li></li>\n";
-							}
-							?>
-							</ul>
-						</div>
-						<script>
-						$(function(){
-							$('.all_cate .allc_bt').click(function(){
-								if($('.all_cate .con_bx').css('display') == 'none'){
-									$('.all_cate .con_bx').show();
-									$(this).html('<i class="ionicons ion-ios-close-empty"></i> 전체카테고리');
-								} else {
-									$('.all_cate .con_bx').hide();
-									$(this).html('<i class="fa fa-bars"></i> 전체카테고리');
-								}
-							});
-						});
-						</script>
-					</div>
-					<div class="gnb_li">
-						<ul>
-						<?php
-						for($i=0; $i<count($gw_menu); $i++) {
-							$seq = ($i+1);
-							$page_url = BV_URL.$gw_menu[$i][1];
-							if(!$default['de_pname_use_'.$seq] || !$default['de_pname_'.$seq])
-								continue;
 
-							echo '<li><a href="'.$page_url.'">'.$default['de_pname_'.$seq].'</a></li>'.PHP_EOL;
-						}
-						?>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<!-- } 상단부 영역 끝 -->
-			<script>
-			$(function(){
-				// 상단메뉴 따라다니기
-				var elem1 = $("#hd_banner").height() + $("#tnb").height() + $("#hd_inner").height();
-				var elem2 = $("#hd_banner").height() + $("#tnb").height() + $("#hd").height();
-				var elem3 = $("#gnb").height();
-				$(window).scroll(function () {
-					if($(this).scrollTop() > elem1) {
-						$("#gnb").addClass('gnd_fixed');
-						$("#hd").css({'padding-bottom':elem3})
-					} else if($(this).scrollTop() < elem2) {
-						$("#gnb").removeClass('gnd_fixed');
-						$("#hd").css({'padding-bottom':'0'})
-					}
-				});
-			});
-			</script>
-		</div>
+  <div id="container">
+    <?php
+		//if(!is_mobile()) { // 모바일접속이 아닐때만 노출
+			//include_once(BV_THEME_PATH.'/quick.skin.php'); // 퀵메뉴
+		//}
 
-		<?php
-		if(defined('_INDEX_')) { // index에서만 실행
-			$sql = sql_banner_rows(0, $pt_id);
-			$res = sql_query($sql);
-			$mbn_rows = sql_num_rows($res);
-			if($mbn_rows) {
-		?>
-		<!-- 메인 슬라이드배너 시작 { -->
-		<div id="mbn_wrap">
-			<?php
-			$txt_w = (100 / $mbn_rows);
-			$txt_arr = array();
-			for($i=0; $row=sql_fetch_array($res); $i++)
-			{
-				if($row['bn_text'])
-					$txt_arr[] = $row['bn_text'];
-
-				$a1 = $a2 = $bg = '';
-				$file = BV_DATA_PATH.'/banner/'.$row['bn_file'];
-				if(is_file($file) && $row['bn_file']) {
-					if($row['bn_link']) {
-						$a1 = "<a href=\"{$row['bn_link']}\" target=\"{$row['bn_target']}\">";
-						$a2 = "</a>";
-					}
-
-					$row['bn_bg'] = preg_replace("/([^a-zA-Z0-9])/", "", $row['bn_bg']);
-					if($row['bn_bg']) $bg = "#{$row['bn_bg']} ";
-
-					$file = rpc($file, BV_PATH, BV_URL);
-					echo "<div class=\"mbn_img\" style=\"background:{$bg}url('{$file}') no-repeat top center;\">{$a1}{$a2}</div>\n";
-				}
-			}
-			?>
-		</div>
-		<script>
-		$(document).on('ready', function() {
-			<?php if(count($txt_arr) > 0) { ?>
-			var txt_arr = <?php echo json_encode($txt_arr); ?>;
-
-			$('#mbn_wrap').slick({
-				autoplay: true,
-				autoplaySpeed: 4000,
-				dots: true,
-				fade: true,
-				customPaging: function(slider, i) {
-					return "<span>"+txt_arr[i]+"</span>";
-				}
-			});
-			$('#mbn_wrap .slick-dots li').css('width', '<?php echo $txt_w; ?>%');
-			<?php } else { ?>
-			$('#mbn_wrap').slick({
-				autoplay: true,
-				autoplaySpeed: 4000,
-				dots: true,
-				fade: true
-			});
-			<?php } ?>
-		});
-		</script>
-		<!-- } 메인 슬라이드배너 끝 -->
-		<?php }
-		}
-		?>
-	</div>
-
-	<div id="container">
-		<?php
-		if(!is_mobile()) { // 모바일접속이 아닐때만 노출
-			include_once(BV_THEME_PATH.'/quick.skin.php'); // 퀵메뉴
-		}
-
-		if(!defined('_INDEX_')) { // index가 아니면 실행
-			echo '<div class="cont_inner">'.PHP_EOL;
-		}
+		//if(!defined('_INDEX_')) { // index가 아니면 실행
+			//echo '<div class="cont_inner">'.PHP_EOL;
+		//}
 		?>
