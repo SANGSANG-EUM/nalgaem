6f647e731a0261c56765699cc1893901020f07c7
mrhmh84 <mrhmh@kakao.com>
2023-09-16 02:42:55 +0900

    nalgaem
    0916

---
+++ nalgaem/partner/admin.head.php
@@ -0,0 +1,7 @@
+<?php
+if (!defined('_GNUBOARD_')) {
+    exit;
+}
+require_once G5_PATH . '/head.sub.php';
+?>
+<?php echo $member['mb_id'] ?>님 관리자	
---
+++ nalgaem/partner/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once './_common.php';
+$g5['title'] = '파트너 관리자';
+require_once './admin.head.php';
+require_once './admin.lib.php';
+?>
+
+<?php
+require_once './admin.tail.php';
